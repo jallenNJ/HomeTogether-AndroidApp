327a318c72cb74d9026238396eeb422b8ae9b1bc
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-01-10 21:39:46 -0500

    App is able to sign up and log in

--- app/src/main/java/edu/ramapo/jallen6/hometogether/Login.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/Login.java
@@ -2,9 +2,11 @@ package edu.ramapo.jallen6.hometogether;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,11 +17,14 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView username;
+    private TextView pass;
     private RequestQueue requestQueue;
+    String url =  "http://192.168.1.18:3000/login";
 
 
     @Override
@@ -27,22 +32,106 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        textView = findViewById(R.id.output);
-        Button buttonParse = findViewById(R.id.button);
+        username = findViewById(R.id.loginUserField);
+        pass = findViewById(R.id.loginPassField);
+       // textView = findViewById(R.id.output);
+       // Button buttonParse = findViewById(R.id.button);
 
         requestQueue = Volley.newRequestQueue(this);
-        buttonParse.setOnClickListener(new View.OnClickListener() {
+      /*  buttonParse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 jsonParse();
             }
-        });
+        });*/
 
     }
 
 
-    private void jsonParse(){
-        String url =  "http://192.168.1.101:3000/login";
+
+    public void signUp(View v)  {
+        JSONObject params;
+        try{
+           params = createLoginParams();
+        } catch (Exception e){
+            Toast.makeText(Login.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean obj = response.getBoolean("status");
+                            String msg = response.getString("message");
+
+                            Toast.makeText(Login.this, ("Status is " + obj + "\n Message: " + msg), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            Toast.makeText(Login.this, "Server error", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+
+        requestQueue.add(request);
+
+    }
+
+
+    private JSONObject createLoginParams() throws JSONException{
+        JSONObject params = new JSONObject();
+        params.put("user", username.getText());
+        params.put("pass", pass.getText());
+        return params;
+    }
+
+
+    public void logIn(View v){
+        JSONObject params;
+        try{
+            params = createLoginParams();
+        } catch (Exception e){
+            Toast.makeText(Login.this, "Log in failed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean obj = response.getBoolean("status");
+                            String msg = response.getString("message");
+
+                            Toast.makeText(Login.this, ("Status is " + obj + "\n Message: " + msg), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            Toast.makeText(Login.this, "Server error", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+
+        requestQueue.add(request);
+    }
+
+
+ /*   private void jsonParse(){
+
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, null,
                 new Response.Listener<JSONObject>() {
@@ -69,5 +158,5 @@ public class Login extends AppCompatActivity {
 
         requestQueue.add(request);
 
-    }
+    }*/
 }
