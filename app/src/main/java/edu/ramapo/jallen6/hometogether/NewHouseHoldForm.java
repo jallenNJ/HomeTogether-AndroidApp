47c3a0a97f8a773c70dd364b58499fd47cfacc54
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-01-11 21:07:54 -0500

    Added framework to new household activity

---
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/NewHouseHoldForm.java
@@ -0,0 +1,21 @@
+package edu.ramapo.jallen6.hometogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewHouseHoldForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_house_hold_form);
+    }
+
+
+
+
+    public void createHouseHold(View v){
+
+    }
+}
