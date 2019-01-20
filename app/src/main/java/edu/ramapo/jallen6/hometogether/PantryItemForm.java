c8200ffa22f468ae7639552e19f60f08e746abdd
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-01-19 22:27:15 -0500

    Added client side courtesy validation on the form

--- app/src/main/java/edu/ramapo/jallen6/hometogether/PantryItemForm.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/PantryItemForm.java
@@ -1,18 +1,23 @@
 package edu.ramapo.jallen6.hometogether;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
+import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
 
-import java.util.Calendar;
-import java.util.Date;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class PantryItemForm extends AppCompatActivity
         implements DatePickerDialog.OnDateSetListener{
@@ -24,7 +29,7 @@ public class PantryItemForm extends AppCompatActivity
         setContentView(R.layout.activity_pantry_item_form);
 
 
-       findViewById(R.id.textView5).setOnClickListener(new View.OnClickListener() {
+       findViewById(R.id.pantryItemFormExpiresLayout).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DialogFragment newFragment = new DatePickerFragment();
@@ -34,15 +39,73 @@ public class PantryItemForm extends AppCompatActivity
 
             }
         });
+    }
+
+    public void submitForm(View v){
+        TextView buffer;
+        int[] formIds = {R.id.pantryItemFormNameField, R.id.pantryItemFormQuantityField,
+                R.id.pantryItemFormExpiresField, R.id.pantryItemFormCategoryField,
+                R.id.pantryItemFormTagField};
+
+        String[] jsonKeys = {"Name", "Quantity", "Expires", "Category", "Tag"};
+
+
+        JSONObject params = new JSONObject();
+        for(int i = 0; i < formIds.length; i++){
+            buffer = findViewById(formIds[i]);
+            String fieldData = buffer.getText().toString().trim();
+            if (fieldData.equalsIgnoreCase("")) {
+                buffer.setError("This field can not be blank");
+                return;
+            }
+            try{
+                params.put(jsonKeys[i], fieldData);
+            } catch (JSONException e){
+                e.printStackTrace();
+                Toast.makeText(this, "Internal error", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        }
+
+
+        String url = NetworkManager.getHostAsBuilder().appendPath("household")
+                .appendPath("pantry").toString();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String message = "Server error";
+                        try {
+                            if(response.getBoolean("status")){
+                                Intent intent = new Intent();
+                                intent.putExtra("id", response.getString("_id"));
+                                setResult(Activity.RESULT_OK, intent);
+                                finish();
+                            }
+                            message = response.getString("message");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(PantryItemForm.this, message, Toast.LENGTH_SHORT).show();
+                }
+                }, NetworkManager.generateDefaultErrorHandler() );
+
+        NetworkManager.getInstance(this).addToRequestQueue(request);
 
     }
 
-    public void onDateSet(DatePicker view, int year, int month, int day){
+
+
+
+public void onDateSet(DatePicker view, int year, int month, int day){
         setDateField((Integer.toString(month) + " " + Integer.toString(day) + ", " + Integer.toString(year)));
     }
 
-    public void setDateField(String text){
-        TextView dateField = findViewById(R.id.textView5);
+    private  void setDateField(String text){
+        TextView dateField = findViewById(R.id.pantryItemFormExpiresField);
         if(dateField == null){
             return;
         }
