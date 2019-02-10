af450816bf4aab11ea2bd1646484dfcf88e2263b
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-02-09 20:59:17 -0500

    Pantry item is able to intialize itself from a JSON object

--- app/src/main/java/edu/ramapo/jallen6/hometogether/PantryItem.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/PantryItem.java
@@ -1,24 +1,39 @@
 package edu.ramapo.jallen6.hometogether;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Observable;
 
 public class PantryItem extends Observable {
 
     private String name;
-    private int quanity;
+    private int quantity;
     private String category;
     private String[] tags;
     //private something Data
     private boolean selected;
 
-    public PantryItem(String n, int q, String c, String[] t){
-          name = n;
-          quanity = q;
-          category=c;
-          tags = t;
+
+    public PantryItem(JSONObject jsonPantry) throws JSONException {
+        name = jsonPantry.getString("name");
+        quantity = jsonPantry.getInt("quantity");
+        category = jsonPantry.getString("category");
+        JSONArray tagsArray = jsonPantry.getJSONArray("tags");
+        //TODO, check what happens if size is zero
+        tags = new String[tagsArray.length()];
+        for(int i =0; i< tagsArray.length(); i++){
+            tags[i] = tagsArray.getString(i);
+        }
     }
 
-
+   // public PantryItem(String n, int q, String c, String[] t){
+    //      name = n;
+    //      quanity = q;
+    //      category=c;
+    //      tags = t;
+   // }
 
     boolean isSelected(){
         return selected;
@@ -27,12 +42,6 @@ public class PantryItem extends Observable {
     public void toggleSelected(){
         selected = !selected;
     }
-
-
-
-
-
-
 
 
 }
