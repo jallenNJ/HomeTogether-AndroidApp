81b80815b87ec36d60e8c522c074f5f4921572a3
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-04-17 14:17:01 -0400

    Implemented the swipe Detectors

--- app/src/main/java/edu/ramapo/jallen6/hometogether/SwipeHorizontalDetector.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/SwipeHorizontalDetector.java
@@ -3,8 +3,26 @@ package edu.ramapo.jallen6.hometogether;
 import android.view.MotionEvent;
 
 public class SwipeHorizontalDetector extends SwipeDirectionDetector {
+
+    private static final SWIPE_DIRECTION left = SWIPE_DIRECTION.DIRECTION1;
+    private static final SWIPE_DIRECTION right = SWIPE_DIRECTION.DIRECTION2;
+
     @Override
     public SWIPE_DIRECTION detect(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        //Ensure they didn't go too far off path
+        if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH ||
+                Math.abs(velocityX) < SWIPE_THRESHOLD_VELOCITY) // and not too slow
+            return SWIPE_DIRECTION.NONE;
+        // right to left swipe
+        if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE ) {
+            return left;
+
+        }
+        //left to right swipe
+        if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE) {
+            return right;
+        }
+
         return SWIPE_DIRECTION.NONE;
     }
 }
--- app/src/main/java/edu/ramapo/jallen6/hometogether/SwipeVerticalDetector.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/SwipeVerticalDetector.java
@@ -3,8 +3,27 @@ package edu.ramapo.jallen6.hometogether;
 import android.view.MotionEvent;
 
 public class SwipeVerticalDetector extends SwipeDirectionDetector {
+
+    private static final SWIPE_DIRECTION up = SWIPE_DIRECTION.DIRECTION1;
+    private static final SWIPE_DIRECTION down = SWIPE_DIRECTION.DIRECTION2;
+
     @Override
     public SWIPE_DIRECTION detect(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            //Ensure they didn't go too far off path or too slow
+            if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH
+                    || Math.abs(velocityY) < SWIPE_THRESHOLD_VELOCITY)
+                return SWIPE_DIRECTION.NONE;
+            // Up swipe
+            if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE ) {
+                return up;
+            }
+
+            if(e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE ){
+                return down;
+            }
+
+
         return SWIPE_DIRECTION.NONE;
+
     }
 }
