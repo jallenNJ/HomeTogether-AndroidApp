9158dc927f7adc043c09f1f94091d904334221be
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-04-17 14:09:25 -0400

    Fixed typo

--- app/src/main/java/edu/ramapo/jallen6/hometogether/SwipeVerticalDetector.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/SwipeVerticalDetector.java
@@ -2,7 +2,7 @@ package edu.ramapo.jallen6.hometogether;
 
 import android.view.MotionEvent;
 
-public class SwipeVerticalDetectpr extends SwipeDirectionDetector {
+public class SwipeVerticalDetector extends SwipeDirectionDetector {
     @Override
     public SWIPE_DIRECTION detect(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         return SWIPE_DIRECTION.NONE;
