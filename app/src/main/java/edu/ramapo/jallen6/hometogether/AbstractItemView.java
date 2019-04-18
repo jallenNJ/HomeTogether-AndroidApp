be28bccf23505717d35a23c38169fe892bb02da7
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-04-18 17:31:49 -0400

    Updated Android Studios, Gradle, and Kotlin Version

--- app/src/main/java/edu/ramapo/jallen6/hometogether/AbstractItemView.java
+++ app/src/main/java/edu/ramapo/jallen6/hometogether/AbstractItemView.java
@@ -7,8 +7,6 @@ import android.graphics.Color;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
